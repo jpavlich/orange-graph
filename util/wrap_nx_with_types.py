c2073bc6260748b59a0aedc6ff2e476dc6405d3c
Jaime A. Pavlich-Mariscal <jaime.pavlich@gmail.com>
2020-02-11 10:50:53 -0500

    wrapper template created

--- util/wrap_nx_with_types.py
+++ util/wrap_nx_with_types.py
@@ -1,11 +1,25 @@
 import networkx as nx
 import util.nx_extract as nxe
 from inspect import getmembers, isfunction, signature
+from util.nx_extract import get_member_metadata, FunctionMetadata
+from typing import *
+from jinja2 import Template
+
+
+def load_template(filename):
+    with open(filename) as f:
+        return Template(f.read())
+
+
+def wrap_function(fun_md: FunctionMetadata):
+    pass
+
 
 if __name__ == "__main__":
+    types: Set = set({})
+    fun_template = load_template("util/wrapped_fun.py.j2")
     for m in getmembers(nx):
-        if isfunction(m[1]):
-            args = signature(m[1])
-            print(args)
-            md = nxe.get_member_metadata(m)
-            print(md)
+        md = get_member_metadata(m)
+        if md:
+            print(fun_template.render(fun=md))
+            # for param, desc in md.signature.items():
